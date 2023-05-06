68a0997d45016a985a00999491557a11e2b57b19
SFMZ21 <smartinezz1@miumg.edu.gt>
2023-05-05 21:34:13 -0600

    Iniciando proyecto

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+locals{
+    prefix="sf-terraform"
+    env="dev"
+    aws={
+        region= "us-east-1"
+    }
+
+    vpc={
+        cidr="10.0.0.0/16"
+        cidr_subnet_public="10.0.0.0/24"
+        cidr_subnet_private="10.0.1.0/24"
+    }
+}
